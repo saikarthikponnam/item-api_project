e63f549bc5f825cd5ea48ee46866e909864c6417
ponnamkeerthi2001 <78363147+ponnamkeerthi2001@users.noreply.github.com>
2026-02-11 12:16:47 +0530

    Initial commit - Item API project

---
+++ src/main/java/com/karthik/itemapi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.karthik.itemapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Item API is running successfully!";
+    }
+}
